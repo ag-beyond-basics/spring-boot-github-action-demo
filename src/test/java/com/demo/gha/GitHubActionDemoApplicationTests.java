c93903bb49bc1363bea15fd9c9329d61c69527a3
ag-beyond-basics <kuashish1988@gmail.com>
2023-02-05 14:34:51 +0530

    updated

---
+++ src/test/java/com/demo/gha/GitHubActionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.gha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
